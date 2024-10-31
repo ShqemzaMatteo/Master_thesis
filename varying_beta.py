46fd691e7e94e58c44f2567edc9f6800f011b8c5
Matteo Shqemza <matteoshqemza@gmail.com>
2024-10-31 17:04:54 +0100

    varying beta

--- varying_beta.py
+++ varying_beta.py
@@ -7,7 +7,7 @@ random.seed(None)
 
 #constants
 N_dim = 50
-beta = np.arange(0,36)
+beta = np.linspace(0,10,0)
 
 #adjacency matrix for a ring
 Adjacency = np.zeros((N_dim, N_dim))
@@ -16,6 +16,15 @@ for i in range(N_dim-1):
     Adjacency[i+1, i] = 1
 Adjacency[N_dim-1, 0] = 1
 Adjacency[0, N_dim-1] = 1
+for i in range(N_dim):
+    sum = 0
+    for j in range(N_dim):
+        sum += Adjacency[i, j]
+    for k in range(N_dim):
+        Adjacency[i, k] /= sum
+# laplacian 
+Laplacian = np.identity(N_dim) - Adjacency
+
 # e-r adjacency matrix
 def er_adjacency_matrix(n, p):
     adjacency = np.zeros((n, n))
@@ -26,6 +35,14 @@ def er_adjacency_matrix(n, p):
                 adjacency[j,i] = 1
     return adjacency
 Adjacency_er = er_adjacency_matrix(N_dim, 0.7)
+for i in range(N_dim):
+    sum = 0
+    for j in range(N_dim):
+        sum += Adjacency_er[i, j]
+    for k in range(N_dim):
+        Adjacency_er[i, k] /= sum
+# laplacian 
+Laplacian_er = np.identity(N_dim) - Adjacency_er
 #adjacency matrix for a B-A
 def ba_adjacency_matrix(n,m):
     adjacency = np.zeros((n,n))
@@ -48,43 +65,30 @@ def ba_adjacency_matrix(n,m):
             degrees[target] += 1
     return adjacency
 Adjacency_ba = ba_adjacency_matrix(N_dim, 3)
-
-
-def Laplacian(adjacency):
-    #adjacency normalization
-    for i in range(N_dim):
-        sum = 0
-        for j in range(N_dim):
-            sum += adjacency[i, j]
-        for k in range(N_dim):
-            adjacency[i, k] /= sum
-    # laplacian 
-    Laplacian = np.identity(N_dim) - adjacency
-    #diagonalization 
-    Lap_eigenvalue, Lap_eigenvector = np.linalg.eig(Laplacian)
-    idx = Lap_eigenvalue.argsort()[::]    #sort the eigenvalue and eigenstate
-    Lap_eigenvalue = Lap_eigenvalue[idx]
-    Lap_eigenvector = Lap_eigenvector[:,idx]
-    Lap_eigenvector = np.matrix.transpose(Lap_eigenvector) #the eigenstate were in the column
-    Lap_eigenvalue = np.diag(Lap_eigenvalue)
-    return Laplacian, Lap_eigenvalue, Lap_eigenvector
+for i in range(N_dim):
+    sum = 0
+    for j in range(N_dim):
+        sum += Adjacency_ba[i, j]
+    for k in range(N_dim):
+        Adjacency_ba[i, k] /= sum
+# laplacian 
+Laplacian_ba = np.identity(N_dim) - Adjacency_ba
 
 #entropy
-def Von_Neumann(b, adjacency):
-    Laplacian, _, _ = Laplacian(adjacency)
-    density_matrix = sc.expm(-b*Laplacian)
+def Von_Neumann(b, laplacian):
+    density_matrix = sc.expm(-b*laplacian)
     density_matrix /= np.trace(density_matrix)
     return -np.trace(density_matrix @ sc.logm(density_matrix))
 
 #plot
-y_1 = np.vectorize(lambda t: Von_Neumann(t,Adjacency))(beta)
-y_2 = np.vectorize(lambda t: Von_Neumann(t,Adjacency_er))(beta)
-y_3 = np.vectorize(lambda t: Von_Neumann(t,Adjacency_ba))(beta)
+#y_1 = np.vectorize(lambda t: Von_Neumann(t,Laplacian))(beta)
+y_2 = np.vectorize(lambda t: Von_Neumann(t,Laplacian_er))(beta)
+y_3 = np.vectorize(lambda t: Von_Neumann(t,Laplacian_ba))(beta)
 
-plt.plot(beta, y_1, label='Boltzmann_weight')
-plt.plot(beta, y_2, label='Boltzmann_weight')
-plt.plot(beta, y_3, label='Boltzmann_weight')
-plt.xlabel('time')
+#plt.plot(beta, y_1, label='Ring')
+plt.plot(beta, y_2, label='E-R')
+plt.plot(beta, y_3, label='B-A')
+plt.xlabel('beta')
 plt.ylabel('entropy')
 plt.ylim((-1 , 5))
 plt.grid()
